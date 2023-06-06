dd303934324629e2b2084b530679470aa5ab4b33
Wneq1 <127328405+Wneq1@users.noreply.github.com>
2023-06-06 16:50:40 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,18 @@
+class Main {
+  public static void main(String[] args) {
+
+    String string =" Siema wariacie co tam u ciebie ?";
+    System.out.println("String"+ string);
+
+char findChar = ' ';
+    int count =0;
+
+    for(int i =0; i<string.length();i++){
+if(string.charAt(i)==findChar){
+  count ++;
+}      
+    }
+    System.out.println("Ilosc szukanej zmiennej "+ count);
+
+  }
+}
